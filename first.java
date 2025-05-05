3ce0ed47a6e7b2401ba088c9fcbcf7cbe046c405
Archana Rai <112776047+ArchanaRaii@users.noreply.github.com>
2025-05-05 23:56:29 +0530

    Create first.java

---
+++ first.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void printItems(int n
+for( i=0;i<10 ;i++ ) 
+    }
+
+
+
+    // DO NOT CHANGE THE MAIN METHOD BELOW
+    public static void main(String[] args) {
+        printItems(10);
+    }
+
+}

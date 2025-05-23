b8b6c7e0bf2e06f1af0b3f75f7e094ccbaeee2e7
Archana Rai <112776047+ArchanaRaii@users.noreply.github.com>
2025-05-23 11:36:49 +0530

    different inputs

--- 05-Big-O(a+b).java
+++ 05-Big-O(a+b).java
@@ -1,1 +1,17 @@
-class main
+public class Main {
+
+   public static void printItems(int a, int b) {
+       for(int i = 0; i < a; i++){
+           System.out.println(i);
+       }
+
+       for(int j = 0; j < b; i++){
+           System.out.println(j);
+       }
+   }
+   
+    public static void main(String[] args) {
+        printItems(10);
+    }
+
+}

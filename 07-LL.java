82cd4392d133fc4b995ed5fef28b43c3ff7830b5
Archana Rai <112776047+ArchanaRaii@users.noreply.github.com>
2025-05-25 21:09:08 +0530

    append method to LL

--- 07-LL.java
+++ 07-LL.java
@@ -81,3 +81,20 @@ public class LinkedList {
 
 }
 
+public class Main {
+
+    public static void main(String[] args) {
+
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.makeEmpty();
+        myLinkedList.append(1);
+        myLinkedList.append(2);
+
+
+        myLinkedList.printAll();
+        
+
+    }
+    
+}
+
